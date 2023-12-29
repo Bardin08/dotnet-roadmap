a95a820b8acc22159874c0a84f97d13e756734a7
Bardin08 <vbardin810@gmail.com>
2023-12-29 02:53:09 +0200

    Drop redundant WriteLines

--- Assignments/Assignment1/TicTacToeProcessor.cs
+++ Assignments/Assignment1/TicTacToeProcessor.cs
@@ -8,9 +8,6 @@ internal class TicTacToeProcessor(
     {
         var players = playersProvider.GetPlayers();
 
-        Console.WriteLine(players.p1.Name + " " + players.p1.Symbol);
-        Console.WriteLine(players.p2.Name + " " + players.p2.Symbol);
-
         var gameProcessor = new GameProcessor(inputProvider, players);
         var gameResult = gameProcessor.Play();
 
