70935e424124b5cdc711f36071fc1d163a8e0b9a
Bardin08 <vbardin810@gmail.com>
2023-12-29 14:12:10 +0200

    Add Leaderboard.cs implementation

---
+++ Assignments/Assignment1/Leaderboard.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace Assignment1;
+
+internal class Leaderboard
+{
+    private readonly Dictionary<string, LeaderboardRecord> _leaderboardRecords = new();
+
+    internal void AddIfNotExists(PlayerDescriptor player)
+    {
+        if (!_leaderboardRecords.ContainsKey(player.Name))
+        {
+            _leaderboardRecords.Add(player.Name, new LeaderboardRecord(player.Name));
+        }
+    }
+
+    internal void Update(PlayerDescriptor player, bool? isWinner)
+    {
+        var record = _leaderboardRecords[player.Name];
+
+        if (isWinner is null)
+            _leaderboardRecords[player.Name] = record with { Draws = record.Draws + 1 };
+        else if (isWinner.Value)
+            _leaderboardRecords[player.Name] = record with { Wins = record.Wins + 1 };
+        else
+            _leaderboardRecords[player.Name] = record with { Loses = record.Loses + 1 };
+    }
+
+    internal void Save()
+    {
+        const string path = "../leaderboard.json";
+        File.WriteAllText(path, JsonSerializer.Serialize(_leaderboardRecords));
+    }
+}
